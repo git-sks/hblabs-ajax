96c631836deb94f63b864ea3e0a44d0f5c3b9b9c
Stephanie Shih <stephk.shih@gmail.com>
2020-05-21 18:27:23 -0700

    Completed event handler for order melons form submit

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -21,7 +21,7 @@ $('#weather-form').on('submit', (evt) => {
 
   const formData = {
     // TODO: select the zipcode input
-    zipcode: $('input#zipcode-field').val()
+    zipcode: $('input#zipcode-field').val(),
   };
 
   // TODO: choose a request method (GET or POST) by uncommenting one of
@@ -46,10 +46,28 @@ $("#order-form").on('submit', (evt) => {
 
   // TODO: create an object to store key-value pairs that'll be sent to
   // the server
+  const formData = {
+    qty: $('input#qty-field').val(),
+    melon_type: $('select#melon-type-field').val(),
+  };
 
   // TODO: make a request to /order-melons
   //
   // In the callback function, use the response from the server to
   // update #order-status. IMPORTANT: if the result code is 'ERROR',
   // make it show up in red.
+  $.post('/order-melons', formData, (response) => {
+    const resultCode = response.code;
+    const resultMsg = response.msg;
+
+    const orderStatus = document.querySelector('div#order-status');
+    orderStatus.innerHTML = `<p>${resultMsg}</p>`;
+
+    if (resultCode === 'ERROR') {
+      orderStatus.setAttribute('style', 'color: red');
+    }
+    else {
+      orderStatus.setAttribute('style', 'color: ""');
+    }
+  });
 });
